cc8422cf7b93bbcb28627b738300c28e075dd6ef
ShruthiR <shruthi.rajasekar@fau.de>
2022-12-05 02:57:18 +0100

    pip installable Structure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gitandpip",
+    name="snowflake",
     version="0.0.1",
     author="Shruthi Rajasekar",
     description="pip installable",
