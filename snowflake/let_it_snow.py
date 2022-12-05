56807fee1bd01f8ca73c699d3f764ffa0c90f98a
ShruthiR <shruthi.rajasekar@fau.de>
2022-12-05 03:02:59 +0100

    pip installable Structure

--- snowflake/let_it_snow.py
+++ snowflake/let_it_snow.py
@@ -3,18 +3,19 @@ import numpy as np
 
 
 def main(speed=0, bg_color="grey"):
+    fileName = 'turtle_image'
     # create Turtle object
     turtle_screen = turtle.Screen()
     myTurtle = turtle.Turtle()
-    
+
     # set speed to 'fastest = 0'
     myTurtle.speed(speed)
     # change background color
     turtle_screen.bgcolor(bg_color)
-  
- 
-    """TODO: define different colors here"""
 
+    """TODO: define different colors here"""
+    colors = ['gold', 'darkgreen', 'red', 'violet', 'lavender', 'black', 'purple', 'navy', 'DarkSalmon', 'skyblue',
+              'coral', 'turquoise', 'lavender']
 
     for _ in range(10):
         # define some params
@@ -22,7 +23,7 @@ def main(speed=0, bg_color="grey"):
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
-
+        myTurtle.color(colors[np.random.randint(0, 13)])
         # Go to the start position of the snowflake
         myTurtle.penup()
         myTurtle.goto(pos[0], pos[1])
@@ -32,6 +33,8 @@ def main(speed=0, bg_color="grey"):
         for _ in range(8):
             snowflake_branch(size, myTurtle)
             myTurtle.left(45)
+
+    turtle_screen.mainloop()
 
 
 def snowflake_branch(size, myTurtle):
